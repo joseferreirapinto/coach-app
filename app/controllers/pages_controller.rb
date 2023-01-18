7773ca2fc86edfddda76da0f0435fa38c40e63d2
jose pinto <josedfpinto@gmail.com>
2023-01-18 14:49:19 +0000

    added first_name and last_name to User

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  # PARA DESCOMENTAR QUANDO USERS ESTIVER PRONTO
+  # skip_before_action :authenticate_user!, only: :home
   def home
   end
 end
